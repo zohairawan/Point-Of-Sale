62e96898ac0681795c1ae42794fc94427ad6cb78
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-09-16 17:34:35 -0700

    Create util Class to return table from db as a list

--- src/main/java/utils/PointOfSaleDB.java
+++ src/main/java/utils/PointOfSaleDB.java
@@ -1,5 +1,54 @@
 package utils;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import storage.Products;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 public class PointOfSaleDB {
 
+    public static List<Products> getTableAsList() {
+        List<Products> products = new ArrayList<>();
+        Properties properties = new Properties();
+        String config = "src/main/resources/point-of-sale.properties";
+        try {
+            properties.load(Files.newInputStream(Path.of(config), StandardOpenOption.READ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setServerName(properties.getProperty("serverName"));
+        dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
+        dataSource.setDatabaseName(properties.getProperty("databaseName"));
+        String query = "SELECT * FROM products";
+
+        try (Connection connection = dataSource.getConnection(
+                properties.getProperty("user"),
+                System.getenv("MYSQL_PASS"));
+             Statement statement = connection.createStatement()
+        ) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                products.add(new Products(
+                                resultSet.getInt("plu"),
+                                resultSet.getString("name"),
+                                resultSet.getDouble("price"),
+                                resultSet.getInt("calculation_code")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        products.forEach(System.out::println);
+        return products;
+    }
 }
